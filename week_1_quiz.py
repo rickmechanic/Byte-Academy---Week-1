defd0d2cdd54398b0120ffc73be160910f0cf3d1
rickmechanic <43411660+rickmechanic@users.noreply.github.com>
2018-10-15 12:32:36 -0400

    Update week_1_quiz.py

--- week_1_quiz.py
+++ week_1_quiz.py
@@ -1,19 +1,22 @@
-with open("currency.txt", "r") as file_object:
-    doc = file_object.readlines()
+def read_currency(filename):
+    with open(filename, "r") as file_object:
+        doc = file_object.readlines()
 
-for line in doc:
-    line = line.split()
+    for line in doc:
+        line = line.split()
 
-currency_list = [{} for i in range(len(doc))]
+    currency_list = [{} for i in range(len(doc))]
 
-x = 0
-for i in currency_list:
-    i["symbol"] = doc[x][0:3]
-    x += 1
+    x = 0
+    for i in currency_list:
+        i["symbol"] = doc[x][0:3]
+        x += 1
 
-x = 0
-for i in currency_list:
-    i["rate"] = doc[x][4:-2:1]
-    x += 1
+    x = 0
+    for i in currency_list:
+        i["rate"] = doc[x][4:-2:1]
+        x += 1
 
-print(currency_list)
+    print(currency_list)
+
+read_currency("currency.txt")
